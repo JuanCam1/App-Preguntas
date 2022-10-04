d59a96c6a6de47b7b2b7ebced80608f87d9f88c3
Juan <juancamilor576@gmail.com>
2022-10-04 16:59:44 -0500

    Finalizado app de preguntas practicando metodos de array y useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import Swal from 'sweetalert2';
 import './App.css';
+import { data } from './data/data';
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const [respuestas, setRespuestas] = useState({
+    radio0: '',
+    radio1: '',
+    radio2: '',
+    radio3: '',
+    radio4: '',
+  });
+
+  const barraProgres = useRef();
+  const dialogInit = useRef();
+
+  useEffect(() => {
+    let counter;
+
+    if (toggle) {
+      counter = setInterval(() => {
+        setTimer((timer) => timer + 5);
+
+        if (timer <= 100) {
+          barraProgres.current.style.width = `${timer}%`;
+        } else {
+          clearInterval(counter);
+          setToggle(false);
+          Swal.fire({
+            title: 'Su tiempo ha finalizado',
+            showDenyButton: true,
+            confirmButtonText: 'Ver resultados',
+            denyButtonText: `Reiniciar`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              results();
+            } else if (result.isDenied) {
+              resert();
+            }
+          });
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(counter);
+  }, [toggle, timer]);
+
+  let choices = data.map((item) => item.choices);
+  let answers = data.map((item) => item.answer);
+  let respuestasValues = Object.values(respuestas);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setRespuestas({ ...respuestas, [name]: value });
+  };
+
+  const results = () => {
+    let contador = 0;
+    for (let i = 0; i < 5; i++) {
+      if (answers[i].toLowerCase() === respuestasValues[i].toLowerCase()) {
+        contador++;
+      }
+    }
+
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: `Tuvo ${contador} respuestas correctas`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+
+    resert();
+  };
+
+  const resert = () => {
+    dialogInit.current.style.display = 'grid';
+    setTimer(0);
+  };
+
+  const handleDialog = () => {
+    dialogInit.current.style.display = 'none';
+    setToggle(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='appProgress'>
+        <div ref={barraProgres} className='appProgressPorc'></div>
+      </div>
+      <div className='appContainer'>
+        <section className='appCard'>
+          <h2 className='appTitle'>Quiz JavaScript</h2>
+          {data.map((item) => {
+            return (
+              <div className='appBody' key={item.id}>
+                <p>
+                  {item.id}. {item.question}
+                </p>
+                <div className='appButtons'>
+                  {choices[item.id - 1].map((btn, index) => {
+                    return (
+                      <div key={index}>
+                        <input
+                          type='radio'
+                          name={`radio${item.id - 1}`}
+                          onChange={handleOnChange}
+                          value={btn}
+                        />
+                        <label htmlFor={`radio${item.id - 1}`}>{btn}</label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+
+      <dialog ref={dialogInit}>
+        <div>
+          <h2>Iniciar cuestionario</h2>
+          <button onClick={handleDialog}>Iniciar</button>
+        </div>
+      </dialog>
+    </main>
   );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    question: "Cual de las siguientes no es un metodo de un array?",
+    choices: ["find", "typeof", "map", "sort"],
+    answer: "typeof",
+  },
+  {
+    id: 2,
+    question: "JavaScript es un lenguaje?",
+    choices: ["Compilado", "Interpretado"],
+    answer: "interpretado",
+  },
+  {
+    id: 3,
+    question: "JavaScript es sensible a mayusculas y minusculas?",
+    choices: ["Verdadero", "Falso"],
+    answer: "verdadero",
+  },
+  {
+    id: 4,
+    question: "if(undefined) es igual a?",
+    choices: ["True", "False"],
+    answer: "false",
+  },
+  {
+    id:5,
+    question: "Que lenguaje no sirve para estilizar sitios web?",
+    choices: [ "Less", "Sass", "CSS", 'NextCSS'],
+    answer: "nextCSS",
+  },
+];
